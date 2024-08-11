684a9e786c6f907a9ffcc01e4ea9b1d491eba59d
Jonas Alder <jonas.alder@gmail.com>
2024-08-11 15:41:50 +0200

    :sparkles: render both tiles with debug points visible

--- ui/display.py
+++ ui/display.py
@@ -52,14 +52,17 @@ ui_state = UiState()
 class TrackRendererCanvas:
     def __init__(self, track):
         self.track = track
+        self.track_color = 'grey'
+        self.render_debug_points = True
+        self.debug_point_size = 2
 
     def generate_js(self, canvas_id="track-canvas"):
         script = f"""
         var canvas = document.getElementById('{canvas_id}');
-        var context = canvas.getContext('2d');
-        context.clearRect(0, 0, canvas.width, canvas.height);
-        context.lineWidth = 2;
-        context.strokeStyle = 'white';
+        var ctx = canvas.getContext('2d');
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
+        ctx.lineWidth = 2;
+        ctx.strokeStyle = 'white';
         """
 
         for tile in self.track.tiles:
@@ -75,39 +78,50 @@ class TrackRendererCanvas:
     def _generate_straight_line(self, tile: StraightTile):
         points = tile.get_defining_points()
         return f"""
-            context.fillStyle = 'blue';
-            context.beginPath();
-            context.arc({tile.origin.x}, {tile.origin.y}, 2, 0, 2 * Math.PI);
-            context.fill();
-            context.fillStyle = 'grey';
-            context.beginPath();
-            context.moveTo({points[0].x}, {points[0].y});
-            context.lineTo({points[1].x}, {points[1].y});
-            context.lineTo({points[3].x}, {points[3].y});
-            context.lineTo({points[2].x}, {points[2].y});
-            context.fill();
-            """
+            ctx.fillStyle = '{self.track_color}';
+            ctx.beginPath();
+            ctx.moveTo({points[0].x}, {points[0].y});
+            ctx.lineTo({points[1].x}, {points[1].y});
+            ctx.lineTo({points[3].x}, {points[3].y});
+            ctx.lineTo({points[2].x}, {points[2].y});
+            ctx.fill();
+            """ + self._generate_debug_poitns(points, 'blue', 'orange') if self.render_debug_points else ""
 
     def _generate_corner(self, tile: CornerTile):
+        points = tile.get_defining_points()
         radius_center = tile.get_radius_center()
-        # TODO: share logic with tile.py
-        radius = tile.width + tile.inner_radius if tile.direction == Direction.RIGHT else tile.inner_radius
-        clockwise = 'true' if tile.direction == Direction.LEFT else 'false'
+        # TODO: make this less stupid and noisy
+        left_radius = tile.width + tile.inner_radius if tile.direction == Direction.RIGHT else tile.inner_radius
+        right_radius = tile.width + tile.inner_radius if tile.direction == Direction.LEFT else tile.inner_radius
+        direction_left = 'true' if tile.direction == Direction.LEFT else 'false'
+        direction_right = 'true' if tile.direction == Direction.RIGHT else 'false'
         rad_offset = math.pi / 2 if tile.direction == Direction.LEFT else -math.pi / 2
         return f"""
-            context.fillStyle = 'red';
-            context.beginPath();
-            context.arc({tile.origin.x}, {tile.origin.y}, 2, 0, 2 * Math.PI);
-            context.fill();
-            context.fillStyle = 'green';
-            context.beginPath();
-            context.arc({radius_center.x}, {radius_center.y}, 2, 0, 2 * Math.PI);
-            context.fill();
-            context.beginPath();
-            context.arc({radius_center.x}, {radius_center.y}, {radius}, {tile.origin.orientation_rad + rad_offset}, {tile.origin.orientation_rad + tile.alpha_rad + rad_offset}, {clockwise});
-            context.stroke();
-            """
+            ctx.fillStyle = '{self.track_color}';
+            ctx.beginPath();
+            ctx.arc({radius_center.x}, {radius_center.y}, {left_radius}, {tile.origin.orientation_rad + rad_offset}, {tile.origin.orientation_rad + tile.alpha_rad + rad_offset}, {direction_left});
+            ctx.lineTo({points[3].x}, {points[3].y});
+            ctx.arc({radius_center.x}, {radius_center.y}, {right_radius},{tile.origin.orientation_rad + tile.alpha_rad + rad_offset}, {tile.origin.orientation_rad + rad_offset},  {direction_right});
+            ctx.fill();
+            """ + self._generate_debug_poitns(points, 'red', 'green') if self.render_debug_points else ""
 
+    def _generate_debug_poitns(self, points, start_color: str, end_color: str):
+        return f"""
+            ctx.fillStyle = '{start_color}';
+            ctx.beginPath();
+            ctx.arc({points[0].x}, {points[0].y}, {self.debug_point_size}, {points[0].orientation_rad - math.pi / 2}, {points[0].orientation_rad + math.pi / 2});
+            ctx.fill();
+            ctx.beginPath();
+            ctx.arc({points[1].x}, {points[1].y}, {self.debug_point_size}, {points[1].orientation_rad - math.pi / 2}, {points[1].orientation_rad + math.pi / 2});
+            ctx.fill();
+            ctx.fillStyle = '{end_color}';
+            ctx.beginPath();
+            ctx.arc({points[2].x}, {points[2].y}, {self.debug_point_size}, {points[2].orientation_rad + math.pi / 2}, {points[2].orientation_rad - math.pi / 2});
+            ctx.fill();
+            ctx.beginPath();
+            ctx.arc({points[3].x}, {points[3].y}, {self.debug_point_size}, {points[3].orientation_rad + math.pi / 2}, {points[3].orientation_rad - math.pi / 2});
+            ctx.fill();
+            """
 
 def TrackView():
     return Div(
